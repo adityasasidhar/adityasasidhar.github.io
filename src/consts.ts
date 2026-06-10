3b9a836389d4b7281269848152b0d262e70450f0
Aditya Sasidhar <telikicherlaadityasasidhar@gmail.com>
2026-06-10 17:47:38 +0530

    first patch

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,14 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'The Deep Field';
+export const SITE_DESCRIPTION = 'A calm, thoughtful space for ideas, writing, and exploration at the edge of knowledge.';
+
+// Author
+export const AUTHOR_NAME = 'Aditya Sasidhar';
+
+// Social links
+export const GITHUB_URL = 'https://github.com/adityasasidhar';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/aditya-sasidhar-2399bb27a/';
+export const KAGGLE_URL = 'https://www.kaggle.com/tadityasasidhar';
+export const ORCID_URL = 'https://orcid.org/0009-0005-6243-1329';
